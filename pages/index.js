f534148517122ba84f8dffddb8b787c0b73c0771
deivisson <deivisson48@yahoo.com.br>
2021-01-26 07:42:05 -0300

    codigo ao final da primeira aula da imersao react

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,36 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import db from '../db.json';
+import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizContainer from '../src/components/QuizContainer'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={({theme}) => theme.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+            <input placeholder="Diz aí seu nome pra jogar :)"/>
+            <button children="Jogar"/>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <h1>Quizes da Galera</h1>
+          <p>Quiz 1</p>
+          <p>Quiz 2</p>
+          <p>Quiz 3</p>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectURL="https://github.com/DeivissonFelipe/aluraquiz-hxh" />
+    </QuizBackground>
+  )
 }
---
+++ pages/quiz.js
@@ -0,0 +1,34 @@
+import db from '../db.json';
+import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizContainer from '../src/components/QuizContainer'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+
+function quiz() {
+    return (
+        <QuizBackground backgroundImage={db.bgHalloween}>
+        <QuizContainer>
+          <QuizLogo />
+          <Widget>
+            <Widget.Header>
+              <h1>{db.title}</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>Quiz</p>
+            </Widget.Content>
+          </Widget>
+  
+          <Widget>
+            <h1>Quizes da Galera</h1>
+            <p>Lorem ipsum dolor sit amet...</p>
+          </Widget>
+          <Footer />
+        </QuizContainer>
+        <GitHubCorner projectURL="#" />
+      </QuizBackground>
+    )
+}
+
+export default quiz
