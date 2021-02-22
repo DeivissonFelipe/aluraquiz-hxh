2ddfb213783cb0d7657a64a7c118ed1525576fc8
deivisson <deivisson48@yahoo.com.br>
2021-02-22 15:25:53 -0300

    Padronizações do lint + adequação e adição das questões no arquivo db

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import NextLink from 'next/link';
+import PropTypes from 'prop-types';
 
 export default function Link({ children, href, ...props }) {
   return (
     <NextLink href={href} passHref>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <a {...props}>
         {children}
       </a>
     </NextLink>
   );
 }
+
+Link.propTypes = {
+  children: PropTypes.node.isRequired,
+  href: PropTypes.string.isRequired,
+};
